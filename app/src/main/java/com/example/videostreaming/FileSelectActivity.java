8259f80087b8709ba5c52cfe4599a193211da7f2
ojasskapre <ojas.kapre@somaiya.edu>
2019-03-17 11:06:33 +0530

    Can select files to play, working for certain folder

--- app/src/main/java/com/example/videostreaming/FileSelectActivity.java
+++ app/src/main/java/com/example/videostreaming/FileSelectActivity.java
@@ -1,7 +1,10 @@
 package com.example.videostreaming;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,11 +19,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class FileSelectActivity extends AppCompatActivity {
     Button btnSend, btnReceive;
     OutputStream outputStream;
-    private String file_name = "/test.mp3";
+    private String file_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,9 @@ public class FileSelectActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Streaming file", Toast.LENGTH_SHORT).show();
-                sendVideoFile();
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                startActivityForResult(intent, 7);
             }
         });
 
@@ -54,14 +61,29 @@ public class FileSelectActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 7:
+                if (resultCode == RESULT_OK) {
+                    final String[] split = data.getData().getPath().split(":");//split the path.
+                    file_name = split[1];
+                    Log.e("FiLEPATHkjhhhhhh", file_name );
+                    Toast.makeText(FileSelectActivity.this, file_name, Toast.LENGTH_LONG).show();
+                    sendVideoFile();
+                }
+                break;
+        }
+    }
+
     private void sendVideoFile() {
-        String internal_storage_path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
-        String full_file_path = internal_storage_path + file_name;
+//        String internal_storage_path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+        String full_file_path = file_name;
         Log.e("FILE_PATH", full_file_path);
         File file = new File(full_file_path);
         int size = (int) file.length();
         final byte[] bytes = new byte[size];
-
+        file_name = "";
         try {
             Log.e("FILE_READ", "File read start");
             BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
--- app/src/main/java/com/example/videostreaming/VideoDataSource.java
+++ app/src/main/java/com/example/videostreaming/VideoDataSource.java
@@ -30,7 +30,7 @@ public class VideoDataSource extends MediaDataSource {
                 int count = 0;
                 ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
                 int read = 0;
-                int x = 307200;
+                int x = 2048000;
                 boolean flag = true;
                 while (read != -1){ //While there is more data
                     //Read in bytes to data buffer
@@ -40,7 +40,7 @@ public class VideoDataSource extends MediaDataSource {
                     byteArrayOutputStream.write(read);
 
                     if (count > x || read == -1){
-                        x = 25600;
+                        x = 250000;
                         Log.e("FILE_READ", "READing from output stream");
                         byteArrayOutputStream.flush();
                         byte[] temp = byteArrayOutputStream.toByteArray();
